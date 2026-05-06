1c59ac191c09e9cb194713e64a17088aece3f576
yakionimi <yakionimi@gmail.com>
2026-05-06 21:37:16 +0900

    Update register-commands.js

--- register-commands.js
+++ register-commands.js
@@ -10,18 +10,36 @@ const commands = [
   new SlashCommandBuilder()
     .setName("tier")
     .setDescription("Tierを設定")
-    .addUserOption(o=>o.setName("player").setDescription("対象").setRequired(true)),
+    .addUserOption(o =>
+      o.setName("player")
+       .setDescription("対象")
+       .setRequired(true)
+    ),
 
   new SlashCommandBuilder()
     .setName("profile")
     .setDescription("Tier確認")
-    .addUserOption(o=>o.setName("player").setDescription("対象").setRequired(true)),
+    .addUserOption(o =>
+      o.setName("player")
+       .setDescription("対象")
+       .setRequired(true)
+    ),
 
   new SlashCommandBuilder()
     .setName("init-top")
-    .setDescription("ランキング更新")
+    .setDescription("ランキング更新"),
 
-].map(c=>c.toJSON());
+  // 👇 これ追加
+  new SlashCommandBuilder()
+    .setName("tier-remove")
+    .setDescription("Tier削除（複数選択）")
+    .addUserOption(o =>
+      o.setName("player")
+       .setDescription("対象")
+       .setRequired(true)
+    )
+
+].map(c => c.toJSON());
 
 const rest = new REST({ version:"10" }).setToken(TOKEN);
 
